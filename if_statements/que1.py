fb95ecec962e8d095a3725916166291530ff063a
JoakBuoy <b.joak@alustudent.com>
2022-11-09 08:21:20 -0800

    :robot: automated commit of one or many files

---
+++ if_statements/que1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def func_1():
+    m = int(input())
+if:
+    m % 2 == 0:
+        print('Weird')
+else:        
+    if m % 2 == 1 and 2 <= m <= 5:
+        print('Not Weird')
+    elif m % 2 == 1 and 6 <= m <= 20:
+        print('Weird')
+    elif m > 20:
+        print('Not Weird')
