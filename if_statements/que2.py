49072febd0dac5517cae9224b5e46e24c5967570
uwicyeza christa <109435174+christauwicyeza@users.noreply.github.com>
2022-11-10 15:46:08 +0200

    changes

--- if_statements/que2.py
+++ if_statements/que2.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-month = str("Enter month to check number of days: "))
+month = str(input("Enter month to check number of days: "))
+
 if month == 'February':
     print("The month of {} has 28 or 29 days". format(month))
 elif month in ['April', 'June', 'September', 'November']:
     print("The month of {} has 30 days". format(month))
 else:
-    month in ['January', 'March', 'July', 'August', 'October', 'December']:
-        print("The month of {} has 31 days". format(month))
+    month in ['January', 'March', 'July', 'August', 'October', 'December']
+
+print("The month of {} has 31 days". format(month))
